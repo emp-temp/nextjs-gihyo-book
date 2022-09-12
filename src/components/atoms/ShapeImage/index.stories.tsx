36839c65a4fb2ecd63c0fe2791f9f8ebefb9bf7b
寺尾空 <empty@teraokuus-MacBook-Air-4.local>
2022-09-12 22:44:37 +0900

    first commit

---
+++ src/components/atoms/ShapeImage/index.stories.tsx
@@ -0,0 +1,53 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react'
+import ShapeImage from './index'
+
+export default {
+  title; 'Atom/ShapeImage',
+  argTypes: {
+    shape: {
+      options: ['circle', 'square'],
+      control: {type: 'radio'},
+      defaultValue: 'square',
+      description: '画像の形',
+      table: {
+        type: {summary: 'circle | square'},
+        defaultValue: {summary: 'square'},
+      },
+    },
+  },
+  src: {
+    control: {type: 'text'},
+    description: '画像URL',
+    table: {
+      type: {summary: 'string'},
+    },
+  },
+  width: {
+    control: {type: 'number'},
+    defaultValue: 320,
+    description: '横幅',
+    table: {
+      type: {summary: 'number'},
+    },
+  },
+  height: {
+    control: {type: 'number'},
+    description: '縦幅',
+    defaultValue: 320,
+    table: {
+      type: {summary: 'number'},
+    },
+  },
+} as ComponentMeta<typeof ShapeImage>
+
+const Template: ComponentStory<typeof ShapeImage> = (args) => (
+  <ShapeImage {...args} />
+)
+
+// 円形
+export const Circle = Template.bind({})
+Circle.args = {src: '/images/sample/1.jpg', shape: 'circle'}
+
+// 四角形
+export const Square = Template.bind({}) 
+Square.args = {src: '/images/sample/1.jpg', shape: 'square'}
